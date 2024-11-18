39eb7908d38472d0352aaf208dca5a92bac91da5
VIKAS <vikasraw17@gmail.com>
2024-11-18 22:41:16 +0530

    commit

--- e-canteen/app/(tabs)/index.tsx
+++ e-canteen/app/(tabs)/index.tsx
@@ -4,9 +4,7 @@ import AppNavigator from '../../navigation/AppNavigator';
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer>
       <AppNavigator />
-    </NavigationContainer>
   );
 };
 
